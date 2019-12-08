87082a961ce088627cd4b4329ed3d820c3f05095
yuyurun <yuyu1.run@icloud.com>
2019-12-08 13:37:54 +0900

    Good-Tuning+kaaztだけど、nの計算違う

--- src/main.py
+++ src/main.py
@@ -30,13 +30,54 @@ def get_bi_word(lines):
     return words
 
 
+def get_uni_word(lines):
+
+    words = []
+    for line in lines:
+        for w in line.replace('\n', '').split():
+            words.append(w)
+
+    return words
+
+
 def count_bi_word(lines):
     words = get_bi_word(lines)
     c = collections.Counter(words)
 
-    count_bi = [0 for i in range(SIZE)]
+    count_count = {}
     for i, co in c.items():
-        count_bi[int(i)] = co/len(words)
+        if co in count_count.keys():
+            count_count[co] += 1
+        else:
+            count_count[co] = 1
+
+    # 最尤推定
+    freq = sorted(count_count.keys())
+    print(freq)
+
+    count_bi = [0 for i in range(SIZE)]
+    # count_bi = [count_count[1]/(SIZE-len(words))/len(words)
+    #            for i in range(SIZE)]
+    for i, co in c.items():
+        if freq.index(co) != len(freq)-1:
+            after = freq[freq.index(co)+1]
+        else:
+            after = co
+        count_bi[int(i)-1] = (after)*count_count[after] / \
+            count_count[co]/len(words)
+
+    # katz
+    uni_word_no_bi = [int(g)
+                      for g in get_uni_word(lines) if not str(g) in words]
+    print(len(set(words)))
+    print(len(set(uni_word_no_bi)))
+
+    total = sum(count_bi)
+#    zero_c = [1 for i in count_bi if i == 0]
+    for w in set(uni_word_no_bi):
+        print(w)
+        count_bi[w-1] = (1-total)*uni_word_no_bi.count(w)/len(uni_word_no_bi)
+
     return count_bi
 
 
